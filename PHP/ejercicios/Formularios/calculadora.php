099ccb399baaaff3977257e1b184af37dfbeb905
4aaron <90799313+4aaron@users.noreply.github.com>
2021-10-21 09:36:15 +0200

    Add files via upload

--- PHP/ejercicios/Formularios/calculadora.php
+++ PHP/ejercicios/Formularios/calculadora.php
@@ -2,28 +2,20 @@
 
 	echo "<h3>CALCULADORA</h3>";
 	
-	if (empty($_POST['op1' || 'op2'])) {
-		// En caso de que no se introduzcan los 2 operandos
-		echo "Introduce dos números para operar";
-	} else {
-		// Cuando se pulse el botón de Enviar
-		if ($_POST['enviar']) {
-			echo "Resultado de operación ";
-			if ($_POST['operacion'] == '+'){
-				echo $_POST['op1'] . " + " . $_POST['op2'] . " = ";
-				echo $_POST['op1'] + $_POST['op2'];
-			} else if ($_POST['operacion'] == '-') {
-				echo $_POST['op1'] . " - " . $_POST['op2'] . " = ";
-				echo $_POST['op1'] - $_POST['op2'];
-			} else if ($_POST['operacion'] == '*') {
-				echo $_POST['op1'] . " * " . $_POST['op2'] . " = ";
-				echo $_POST['op1'] * $_POST['op2'];
-			} else if ($_POST['operacion'] == '/') {
-				echo $_POST['op1'] . " / " . $_POST['op2'] . " = ";
-				echo $_POST['op1'] / $_POST['op2'];
-			} else {
-				echo "Vacio";
-			}
+	if ($_POST['enviar']) {
+		echo "Resultado de operación ";
+		if ($_POST['operacion'] == '+'){
+			echo $_POST['op1'] . " + " . $_POST['op2'] . " = ";
+			echo $_POST['op1'] + $_POST['op2'];
+		} else if ($_POST['operacion'] == '-') {
+			echo $_POST['op1'] . " - " . $_POST['op2'] . " = ";
+			echo $_POST['op1'] - $_POST['op2'];
+		} else if ($_POST['operacion'] == '*') {
+			echo $_POST['op1'] . " * " . $_POST['op2'] . " = ";
+			echo $_POST['op1'] * $_POST['op2'];
+		} else if ($_POST['operacion'] == '/') {
+			echo $_POST['op1'] . " / " . $_POST['op2'] . " = ";
+			echo $_POST['op1'] / $_POST['op2'];
 		}
 	}
 	
