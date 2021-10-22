627bc4739db8804dfa5b33cacbedee8784356792
4aaron <90799313+4aaron@users.noreply.github.com>
2021-10-22 13:32:23 +0200

    Update calculadora1.php

--- PHP/ejercicios/Formularios/calculadora1.php
+++ PHP/ejercicios/Formularios/calculadora1.php
@@ -7,11 +7,8 @@
 			echo "No ha introducido un valor para el operando 2";
 			} else if (isset($_POST['enviar'])) {
 				if (isset($_POST['operacion']) && $_POST['operacion'] == '+') {
-					
 					echo "Operando 1: " . "<span style='border: 1.5px solid; width: 100px; display:block;'>" . $_POST['op1'] . "</span><br><br>";
-					
 					echo "Operando 2: " . "<span style='border: 1.5px solid; width: 100px; display:block;'>" . $_POST['op2'] . "</span><br><br>";
-					
 					$resultado = $_POST['op1'] + $_POST['op2'];
 					echo "Resultado de operación: " . $_POST['op1'] . " " . $_POST['operacion'] . " " . $_POST['op2'] . " = " . $resultado;
 					} else if (isset($_POST['operacion']) && $_POST['operacion'] == '-') {
