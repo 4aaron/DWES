a5916c02059246d7f1d4ce6e63574e03d5b99d9a
4aaron <90799313+4aaron@users.noreply.github.com>
2021-10-22 08:56:50 +0200

    Update calculadora1.php

--- PHP/ejercicios/Formularios/calculadora1.php
+++ PHP/ejercicios/Formularios/calculadora1.php
@@ -1,60 +1,34 @@
+
 <?php
 
 	echo "<h3>CALCULADORA</h3>";
 	
-	echo "Operando 1: ";
-	echo $_POST['op1'];
-	echo "<br>";
-	
-	echo "Operando 2: ";
-	echo $_POST['op2'];
-	echo "<br>";
-	
 	if (empty($_POST['op1'])) {
-		echo "<br>No ha introducido un valor para el operando 1";
-	} else {
-		if (empty($_POST['op2'])) {
-			echo "<br>No ha introducido un valor para el operando 2";
-			} else {
-				if (isset($_POST['enviar'])) {
-					echo "<br>Resultado de operación ";
-					if ($_POST['operacion'] == '+') {
-						echo $_POST['op1'] . " + " . $_POST['op2'] . " = ";
-						echo $_POST['op1'] + $_POST['op2'];
-					} else if ($_POST['operacion'] == '-') {
+		echo "No ha introducido un valor para el operando 1";
+		} else if (empty($_POST['op2'])) {
+			echo "No ha introducido un valor para el operando 2";
+			} else if (isset($_POST['enviar'])) {
+				if (isset($_POST['operacion']) && $_POST['operacion'] == '+') {
+					echo "Resultado de operación ";
+					echo $_POST['op1'] . " + " . $_POST['op2'] . " = ";
+					echo $_POST['op1'] + $_POST['op2'];
+					} else if (isset($_POST['operacion']) && $_POST['operacion'] == '-') {
+						echo "Resultado de operación ";
 						echo $_POST['op1'] . " - " . $_POST['op2'] . " = ";
 						echo $_POST['op1'] - $_POST['op2'];
-					} else if ($_POST['operacion'] == '*') {
-						echo $_POST['op1'] . " * " . $_POST['op2'] . " = ";
-						echo $_POST['op1'] * $_POST['op2'];
-					} else if ($_POST['operacion'] == '/') {
-						echo $_POST['op1'] . " / " . $_POST['op2'] . " = ";
-						echo $_POST['op1'] / $_POST['op2'];
-					} else {
-						echo "<br>No ha seleccionado una operación";
-					}
-				}
-		}
-	
-	
-	/*
-	if ($_POST['enviar']) {
-		echo "Resultado de operación ";
-		if ($_POST['operacion'] == '+') {
-			echo $_POST['op1'] . " + " . $_POST['op2'] . " = ";
-			echo $_POST['op1'] + $_POST['op2'];
-		} else if ($_POST['operacion'] == '-') {
-			echo $_POST['op1'] . " - " . $_POST['op2'] . " = ";
-			echo $_POST['op1'] - $_POST['op2'];
-		} else if ($_POST['operacion'] == '*') {
-			echo $_POST['op1'] . " * " . $_POST['op2'] . " = ";
-			echo $_POST['op1'] * $_POST['op2'];
-		} else if ($_POST['operacion'] == '/') {
-			echo $_POST['op1'] . " / " . $_POST['op2'] . " = ";
-			echo $_POST['op1'] / $_POST['op2'];
-		}
+						} else if (isset($_POST['operacion']) && $_POST['operacion'] == '*') {
+							echo "Resultado de operación ";
+							echo $_POST['op1'] . " * " . $_POST['op2'] . " = ";
+							echo $_POST['op1'] * $_POST['op2'];
+							} else if (isset($_POST['operacion']) && $_POST['operacion'] == '/') {
+								echo "Resultado de operación ";
+								echo $_POST['op1'] . " / " . $_POST['op2'] . " = ";
+								echo $_POST['op1'] / $_POST['op2'];
+								} else {
+									echo "No ha seleccionado una operación";
+								}				
 	}
-	*/
-	echo ("<br><br><a href='calculadora1.html'>Seguir Calculando</a>");
+
+	echo ("<br><br><a href='calculadora.html'>Seguir Calculando</a>");
 	
 ?>
