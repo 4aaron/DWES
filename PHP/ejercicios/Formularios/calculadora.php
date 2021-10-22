4594f9c14adcd98a27af5c2243569418d14e1837
4aaron <90799313+4aaron@users.noreply.github.com>
2021-10-22 12:12:51 +0200

    Update calculadora.php

--- PHP/ejercicios/Formularios/calculadora.php
+++ PHP/ejercicios/Formularios/calculadora.php
@@ -8,26 +8,22 @@
 			echo "No ha introducido un valor para el operando 2";
 			} else if (isset($_POST['enviar'])) {
 				if (isset($_POST['operacion']) && $_POST['operacion'] == '+') {
-					echo "Resultado de operación ";
-					echo $_POST['op1'] . " + " . $_POST['op2'] . " = ";
-					echo $_POST['op1'] + $_POST['op2'];
+					$resultado = $_POST['op1'] + $_POST['op2'];
+					echo "Resultado de operación: " . $_POST['op1'] . $_POST['operacion'] . $_POST['op2'] . " = " . $resultado;
 					} else if (isset($_POST['operacion']) && $_POST['operacion'] == '-') {
-						echo "Resultado de operación ";
-						echo $_POST['op1'] . " - " . $_POST['op2'] . " = ";
-						echo $_POST['op1'] - $_POST['op2'];
+						$resultado = $_POST['op1'] - $_POST['op2'];
+						echo "Resultado de operación: " . $_POST['op1'] . $_POST['operacion'] . $_POST['op2'] . " = " . $resultado;
 						} else if (isset($_POST['operacion']) && $_POST['operacion'] == '*') {
-							echo "Resultado de operación ";
-							echo $_POST['op1'] . " * " . $_POST['op2'] . " = ";
-							echo $_POST['op1'] * $_POST['op2'];
+							$resultado = $_POST['op1'] * $_POST['op2'];
+							echo "Resultado de operación: " . $_POST['op1'] . $_POST['operacion'] . $_POST['op2'] . " = " . $resultado;
 							} else if (isset($_POST['operacion']) && $_POST['operacion'] == '/') {
-								echo "Resultado de operación ";
-								echo $_POST['op1'] . " / " . $_POST['op2'] . " = ";
-								echo $_POST['op1'] / $_POST['op2'];
+								$resultado = $_POST['op1'] / $_POST['op2'];
+								echo "Resultado de operación: " . $_POST['op1'] . $_POST['operacion'] . $_POST['op2'] . " = " . $resultado;
 								} else {
 									echo "No ha seleccionado una operación";
-								}				
+									}
 	}
-
+	
 	echo ("<br><br><a href='calculadora.html'>Seguir Calculando</a>");
 	
 ?>
