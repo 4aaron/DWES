cc44e9c8f4a08af5fdc055c14d9a5d8034bdb0e7
4aaron <90799313+4aaron@users.noreply.github.com>
2021-10-22 13:31:24 +0200

    Update binario.php

--- PHP/ejercicios/Formularios/binario.php
+++ PHP/ejercicios/Formularios/binario.php
@@ -1,8 +1,8 @@
 <?php
 	
 	echo "<h3>CONVERSOR BINARIO</h3>";
-	echo "Número Decimal: " . "<span style='border: 1.5px solid; width: 100px; display:block;'>" . $_POST['decimal'] . "</span><br>";
-	echo "Número Binario: " . "<span style='border: 1.5px solid; width: 100px; display:block;'>";
+	echo "Número Decimal: <span style='border: 1.5px solid; width: 100px; display:block;'>" . $_POST['decimal'] . "</span><br>";
+	echo "Número Binario: <span style='border: 1.5px solid; width: 100px; display:block;'>";
 	echo str_pad((base_convert($_POST['decimal'], 10, 2)), 8, "0", STR_PAD_LEFT) . "</span>";
 	
 	echo ("<br><a href='.'>Volver</a>");
